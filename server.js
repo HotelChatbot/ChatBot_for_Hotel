9e7adf10340234565ed3c6c9a139354c0dba501b
Hu Yao-Chieh <yhuag@connect.ust.hk>
2017-04-09 18:24:24 +0800

    Agent Switch Feature Added and tested; Session tracking feature added, by port number

--- server.js
+++ server.js
@@ -1,34 +1,56 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 // Base socket.io on express app
 var io = require('socket.io')(http);
 // Constanize the port number
 var port = process.env.PORT || 8080;
-// Connection with api.ai
+// Dialog Manager
 var apiai = require('apiai');
-var appAPIAI = apiai("ecc353311a954139b3ff036c8f6eb2ae");
+
+// Global Initialization
+var TOKEN_DemoAgent = "ecc353311a954139b3ff036c8f6eb2ae";
+var TOKEN_ServerTest = "8010c7fae89f4faeb8fe10470ae77742";
 
 // Initialize the front-end
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+app.use(express.static("public"));
 
 // Detect any connection
 io.on('connection', function(socket){
-  console.log("Connected");
-
+  //console.log(socket.handshake.address);
+  //console.log(socket.request.connection.remoteAddress);
+  // Get the client's connection port
+  var portNum = socket.request.connection.remotePort
+  console.log("Connection on port" + portNum);
+  
   // Waiting for the start of the speech recognition
   socket.on('annyang', function(msg){
     console.log("annyang: " + msg);
   });
 
   // Send the userInput to api.ai when notified by front-end
-  socket.on('send_to_apiai', function(msg){
+  socket.on('send_to_apiai', function(arr){
+    // Extract parameters
+    msg = arr[0];
+    isConnectToDemoAgent = arr[1];
+    console.log("Agent Switched: Connect to DemoAgent? " + isConnectToDemoAgent);
+
     console.log("Send to api.ai: " + msg);
+
+    // Connection with api.ai
+    var appAPIAI;
+    if (isConnectToDemoAgent){
+      appAPIAI = apiai(TOKEN_DemoAgent);
+      console.log("DemoAgent connected");
+    }
+    else {
+      appAPIAI = apiai(TOKEN_ServerTest);
+      console.log("ServerTest connected");
+    }
 
     // Send request to api.ai
     var request = appAPIAI.textRequest(msg, {
-      sessionId: '001'
+      sessionId: portNum
     });
 
     // Waiting for the response from api.ai
