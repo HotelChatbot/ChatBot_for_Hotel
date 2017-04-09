29e96983c4cd14f49bfa19ad0fa0b8ec9aecedc2
Hu Yao-Chieh <yhuag@connect.ust.hk>
2017-04-09 20:29:17 +0800

    Initial Commit

---
+++ server.js
@@ -0,0 +1,80 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+// Base socket.io on express app
+var io = require('socket.io')(http);
+// Constanize the port number
+var port = process.env.PORT || 8080;
+// Dialog Manager
+var apiai = require('apiai');
+
+// Global Initialization
+var TOKEN_DemoAgent = "ecc353311a954139b3ff036c8f6eb2ae";
+var TOKEN_ServerTest = "8010c7fae89f4faeb8fe10470ae77742";
+
+// Initialize the front-end
+app.use(express.static("public"));
+
+// Detect any connection
+io.on('connection', function(socket){
+  //console.log(socket.handshake.address);
+  //console.log(socket.request.connection.remoteAddress);
+  // Get the client's connection port
+  var portNum = socket.request.connection.remotePort
+  console.log("Connection on port" + portNum);
+  
+  // Waiting for the start of the speech recognition
+  socket.on('annyang', function(msg){
+    console.log("annyang: " + msg);
+  });
+
+  // Send the userInput to api.ai when notified by front-end
+  socket.on('send_to_apiai', function(arr){
+    // Extract parameters
+    msg = arr[0];
+    isConnectToDemoAgent = arr[1];
+    console.log("Agent Switched: Connect to DemoAgent? " + isConnectToDemoAgent);
+
+    console.log("Send to api.ai: " + msg);
+
+    // Connection with api.ai
+    var appAPIAI;
+    if (isConnectToDemoAgent){
+      appAPIAI = apiai(TOKEN_DemoAgent);
+      console.log("DemoAgent connected");
+    }
+    else {
+      appAPIAI = apiai(TOKEN_ServerTest);
+      console.log("ServerTest connected");
+    }
+
+    // Send request to api.ai
+    var request = appAPIAI.textRequest(msg, {
+      sessionId: portNum
+    });
+
+    // Waiting for the response from api.ai
+    request.on('response', function(response) {
+      var sysOutput = response.result.fulfillment.speech;
+      console.log(sysOutput);
+
+      // Notify the front-end along with the response from api.ai
+      socket.emit("response_from_apiai",sysOutput);
+    });
+
+    request.on('error', function(error) {
+      console.log(error);
+    });
+     
+    request.end();
+
+  });
+
+
+
+});
+
+// Listening on the port
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
