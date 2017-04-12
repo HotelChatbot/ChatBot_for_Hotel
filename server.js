04e9ca7766cefa6540f6e9351fc81e7566c3a84f
khuangaf <huangkh19951228@gmail.com>
2017-04-13 01:52:03 +0800

    try to read csv into server once it's load

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ var http = require('http').Server(app);
 // Base socket.io on express app
 var io = require('socket.io')(http);
 // Constanize the port number
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 80;
 // Dialog Manager
 var apiai = require('apiai');
+var d3 = require('d3');
 
 // Initialize the front-end
 app.use(express.static("public"));
@@ -17,7 +18,8 @@ var TOKEN_DemoAgent = "ecc353311a954139b3ff036c8f6eb2ae";
 var TOKEN_ServerTest = "8010c7fae89f4faeb8fe10470ae77742";
 
 
-var user_data = {}
+var user_data = {};
+var restaurant_data = [];
 
 // Build up the routers
 require('./app/routes.js')(app);
@@ -61,11 +63,13 @@ io.on('connection', function(socket){
       sessionId: portNum
     });
 
+
+    
     //initailize user
     user_data[portNum] = 
-    { 'restaurant_style':null,
-      'restaurant_price':100,
-      'restaurant_name': null
+    { 'restaurant_style':"",
+      'restaurant_price':"",
+      'restaurant_name': ""
     };
     user_data[portNum]['recommend_restaurant'] = new Set()
 
@@ -75,7 +79,11 @@ io.on('connection', function(socket){
       console.log(sysOutput);
       var action =  response.result.action;
       //perform action if needed
-      if(action.length > 0) sysOutput = eval_function(action, response, portNum);
+      if(action.length > 0) {
+        console.log("action",action);
+        sysOutput = eval_function(action, response, portNum);
+        console.log(sysOutput)
+      }
       
       
       // Notify the front-end along with the response from api.ai
@@ -97,6 +105,8 @@ io.on('connection', function(socket){
 // Listening on the port
 http.listen(port, function(){
   console.log('listening on *:' + port);
+  //read db into server
+  read_csv();
 });
 
 
@@ -104,93 +114,97 @@ http.listen(port, function(){
 function eval_function(action, response, portNum){
   switch(action){
     case "recommend_restaurant":
-      recommend_restaurant(response,portNum);
+      response = recommend_restaurant(response,portNum);
       break;
     case "restaurant_give_detail":
-      restaurant_give_detail(response,portNum);
+      response =  restaurant_give_detail(response,portNum);
       break;
   }
+  console.log("eval_function");
   return response
 }
 
 
 
 function recommend_restaurant(response, portNum){
-  console.log(response.result);
+  //console.log(response.result);
   var priceRequest = response.result.parameters['unit-currency'];
   var styleRequest = response.result.parameters["restaurant_style"];
+   
   //update parameter if the response is not empty
-  if(priceRequest.length) user_data[portNum]['restaurant_price'] = priceRequest;
-  if(priceRequest.length) user_data[portNum]['restaurant_style'] = styleRequest;
-  console.log(user_data); 
-
+  if(priceRequest.length > 0) user_data[portNum]['restaurant_price'] = priceRequest;
+  if(styleRequest.length > 0) user_data[portNum]['restaurant_style'] = styleRequest;
+  
+  var currentPrice = "";
+  var currentRestaurant = "";
   //read restaurant csv file
-  d3.csv("/restaurant.csv", function(data) {
-    var currentPrice = 0;
-    var currentRestaurant = null;
-    styleRequest = user_data[portNum]['restaurant_style'];
+  
+  // console.log('styleRequest', styleRequest);
+  restaurant_data.forEach(function(restaurant){
+    var name = restaurant.name;
+    var price = restaurant.price;
+    var style = restaurant.style;
+    var location = restaurant.location;
+    //if the restaurant has not recommended yet
+    if(!user_data[portNum]['recommend_restaurant'].has( name )){
 
-    if(data){
-      //loop over each row of data
-      data.forEach(function(row) {
-        //if the restaurant has not recommended yet
-        if(!user_data[portNum]['recommend_restaurant'].has( row.name )){
-          var price = row.price;
-          //we want to match the restaurant whose price is the closest to the price request
-          if(price < parseInt(priceRequest) && price >currentPrice && row.style == styleRequest){
-            currentPrice = price;
-            currentRestaurant = row.name;
-
-          }
-        }
-      });
-      //no restaurant found
-      if(currentRestaurant == null){
-        //might need to come up with a way to recommend user restaurant with another style
-        //might need to reset context here
-        response = "Sorry we cannot find any restaurant that meets your criteria. What kind of restaurant do you like";
-
-      }
-      else{
-        //add current restaurant to the set so that you don't duplicate recommendation
-        user_data[portNum]['recommend_restaurant'].add(currentRestaurant);
-        //update current user's data
-        user_data[portNum]['restaurant_style'] = styleRequest;
-        user_data[portNum]['restaurant_name'] = currentRestaurant;
-        user_data[portNum]['restaurant_price'] = priceRequest;
-        //concatenate data into response
-        response = "Here is a "+ styleRequest + " restaurant called " + currentRestaurant +"that costs around " +currentPrice;  
-      }
+      //we want to match the restaurant whose price is the closest to the price request
       
+      if( style == styleRequest){
+        
+        if(priceRequest.length == 0 || (parseInt(price) < parseInt(priceRequest) && parseInt(price) >currentPrice)){
+          currentPrice = parseInt(price);
+          currentRestaurant = name;
+           
+        }
+      }
     }
-    else{
-      console.log("no data");   
-    }
-    
   });
+  console.log("currentRestaurant", currentRestaurant);
+  //no restaurant found
+  if(!currentRestaurant){
+    //might need to come up with a way to recommend user restaurant with another style
+    //might need to reset context here
+    console.log("No restaurant");
+    response = "Sorry we cannot find any restaurant that meets your criteria. What kind of restaurant do you like";
+    
+    
+  }
+  else{
+    //add current restaurant to the set so that you don't duplicate recommendation
+    user_data[portNum]['recommend_restaurant'].add(currentRestaurant);
+    //update current user's data
+    user_data[portNum]['restaurant_style'] = styleRequest;
+    user_data[portNum]['restaurant_name'] = currentRestaurant;
+    user_data[portNum]['restaurant_price'] = priceRequest;
+    console.log(user_data);
+    //concatenate data into response
+    response = "Here is a "+ styleRequest + " restaurant called " + currentRestaurant +" that costs around " +currentPrice;  
+  }
+  
+  
   return response;
 }
 
 function restaurant_give_detail(response, portNum){
-  d3.csv("/restaurant.csv", function(data) {
-    //get current user style request from the previously stored user_data
-    restaurant_name = user_data[portNum]['restaurant_name'];
-    if(!restaurant_name) return "Error! have not specify restaurant";
-    if(data){
-      //loop over each row of data
-      data.forEach(function(row) {
-        //if the restaurant has not recommended yet
-        if(row.name == restaurant_name){
-          response = "the restaurant is located at " + row.location + "and it costs you around "+ currentPrice;
-          
-        }
-      });
+  console.log(user_data);
+  restaurant_data.forEach(function(restaurant){
+    var name = restaurant.name;
+    var price = restaurant.price;
+    var style = restaurant.style;
+    var location = restaurant.location;
+    //if the restaurant has not recommended yet
+    if(!user_data[portNum]['recommend_restaurant'].has( name )){
+
+      console.log(name, user_data[portNum]['restaurant_name']);
+      if(name == user_data[portNum]['restaurant_name']){
+        response = "the restaurant is located at " + location + "and it costs you around "+ price;
+      }
+      
+      
     }
-    else{
-      console.log("no data");   
-    }
-    
   });
+  
   return response;
 }
 
@@ -200,4 +214,31 @@ function parse_price(unit_currency){
     return unit_currency.currency;
   }
   else return unit_currency;
+}
+
+function read_csv(){
+  d3.csv("/restaurant.csv", function(data) {
+    if(data){
+      //console.log(data);
+      var i=0;
+      data.forEach(function(row) {
+        var restaurant_name = row.name;
+        var restaurant_stlye = row.style;
+        var price = row.price;
+        var location = row.location;
+        
+        
+        restaurant = {
+          'name': restaurant_name,
+          'style': restaurant_stlye,
+          'price': price,
+          'locatoin': location
+        }
+        restaurant_data.push(restaurant);    
+      });  
+    }
+   console.log(restaurant_data);
+  });
+  
+
 }
